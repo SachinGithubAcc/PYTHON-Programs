3d49178eaf333ba1249153366d7587809244fd32
SachinGithubAcc <127919490+SachinGithubAcc@users.noreply.github.com>
2023-12-21 14:25:15 +0530

    string literals
    demonste the use of string literals

---
+++ string literals.py
@@ -0,0 +1,8 @@
+#program to demonste the use of string literals
+string = 'This is a sring in single quotes'
+char = "c"
+multi_string = '''This is multi-line string with more than one line'''
+
+print(string)
+print(char)
+print(multi_string)
