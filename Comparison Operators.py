e340728a45a22be1ea949093d7d9608963422d75
SachinGithubAcc <127919490+SachinGithubAcc@users.noreply.github.com>
2023-12-21 14:55:55 +0530

    Comparison Operators or Relation Operators
    All Comparison Operators or Relation Operators  used.

---
+++ Comparison Operators.py
@@ -0,0 +1,22 @@
+#All Arithmetic Operation Use.
+
+a = int(input("Enter a number : "))
+b = int(input("Enter b number : "))
+
+less = a < b
+print("Less than number : ",less)
+
+greater = a > b
+print("Greater number : ",greater)
+
+equal = a == b
+print("Equal number : ",equal)
+
+Not_eq = a != b
+print("Not Equal number : ",Not_eq)
+
+gret_equ = a >= b
+print("Greater than or equal : ",gret_equ)
+
+less_eq = a <= b
+print("Less than or equal : ",less_eq)
