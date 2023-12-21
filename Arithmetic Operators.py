b46d0626d4df218d4a4373f1a1a9f3f829f2fe86
SachinGithubAcc <127919490+SachinGithubAcc@users.noreply.github.com>
2023-12-21 14:41:14 +0530

    Arithmetic Operators
    Program to using Arithmetic Operator

---
+++ Arithmetic Operators.py
@@ -0,0 +1,25 @@
+#All Arithmetic Operation Use.
+
+a = int(input("Enter a 1st number : "))
+b = int(input("Enter a 2nd number : "))
+
+add = a + b
+print("Addition of two number : ",add)
+
+sub = a - b
+print("SUbstraction of two number : ",sub)
+
+mul = a * b
+print("Multiplication of two number : ",mul)
+
+div = a / b
+print("Division of two number ",div)
+
+modu = a % b
+print("Modulus two number : ",modu)
+
+floor = a // b
+print("Floor two number : ",floor)
+
+expo = a ** b
+print("Exponent of two number : ",expo)
