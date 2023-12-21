87b76a826f73b397da523511e459265bc958d122
SachinGithubAcc <127919490+SachinGithubAcc@users.noreply.github.com>
2023-12-21 14:14:58 +0530

    Average_No.
    average the given number

---
+++ Finde the Average  given number.py
@@ -0,0 +1,7 @@
+#Write a Python Program to Calculate the Average of Numbers in a Given List.
+
+numbers = [30, 55, 3, 10, 2]
+
+average = sum(numbers)/len(numbers)
+
+print("Average of list: ", round(average,3))
